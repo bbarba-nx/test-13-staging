443a3fa581f5a4247c1dd1afd971d7b9df210aee
Brandon Barba <brandon@nrwl.io>
2024-08-29 08:54:42 -0400

    initial commit

---
+++ apps/test-13-staging/jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  displayName: 'test-13-staging',
+  preset: '../../jest.preset.js',
+  transform: {
+    '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
+    '^.+\\.[tj]sx?$': 'babel-jest',
+  },
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
+  coverageDirectory: '../../coverage/apps/test-13-staging',
+};
---
+++ apps/test-13-staging/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      <NxWelcome title="test-13-staging" />
+      <div />
+    </>
+  );
+}
+
+export default App;
